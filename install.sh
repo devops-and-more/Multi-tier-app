740873140c99993f1d7b3d7003941b5b4533feaa
devops-and-more <devopsandmoore@gmail.com>
2024-09-29 19:27:08 +0200

    +x to install.sh

--- install.sh
+++ install.sh
@@ -3,6 +3,7 @@
 echo -e "We are going to update the site with the latest code from GitHub, press ENTER key to continue, or CTRL-C to quit"
 read key
 
+
 sudo mkdir /etc/mtwa
 sudo mkdir /var/www/html/appdemo
 sudo cp mtwa.conf /etc/mtwa/mtwa.conf
