fbe4667090e871bc3a54b4e8d4c5ad302887f00d
devops-and-more <devopsandmoore@gmail.com>
2024-10-09 23:33:48 +0200

    devops and moore

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-echo -e "We are going to update the site with the latest code from GitHub, press ENTER key to continue, or CTRL-C to quit"
-read key
+#echo -e "We are going to update the site with the latest code from GitHub, press ENTER key to continue, or CTRL-C to quit"
+#read key, we don't need those bcz in dockerizing the app we don't want it to prompt nothing
 
 
 sudo mkdir /etc/mtwa
